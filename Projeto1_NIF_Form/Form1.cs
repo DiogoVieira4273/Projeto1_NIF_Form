24c8d409b91d951aaf1bc4dec48f8e101cfc93de
Diogo Major Vieira <diogomajor1993@outlook.pt>
2023-05-15 17:29:38 +0100

    Add project files.

---
+++ Projeto1_NIF_Form/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Text.Json;
+
+namespace Projeto1_NIF_Form
+{
+    public partial class Form1 : Form
+    {
+        private readonly string key = "7081912941ec5e7715a8de32ef8213d7";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void Gravar_Dados_JSON_Click(object sender, EventArgs e)
+        {
+            Root example = null;
+            string json = "";
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://www.nif.pt/");
+                json = await client.GetStringAsync($"?json=1&q={Inserir_NIF.Text}&key={key}");
+                json = json.Replace($"{Inserir_NIF.Text}\":", "NIF\":");
+                example = JsonSerializer.Deserialize<Root>(json);
+                int NIF_Cliente = example.Records.NIF.Nif;
+                Inserir_NIF.Text = NIF_Cliente.ToString();
+                string Nome_Cliente = example.Records.NIF.Title;
+                Inserir_Nome.Text = Nome_Cliente.ToString();
+                string Adereço = example.Records.NIF.Address;
+                Inserir_Adereço.Text = Adereço.ToString();
+                string PC4 = example.Records.NIF.Place.Pc4;
+                Inserir_PC4.Text = PC4.ToString();
+                string PC3 = example.Records.NIF.Place.Pc3;
+                Inserir_PC3.Text = PC3.ToString();
+                string Distrito = example.Records.NIF.Geo.Region;
+                Inserir_Distrito.Text = Distrito.ToString();
+                string Concelho = example.Records.NIF.Geo.County;
+                Inserir_Concelho.Text = Concelho.ToString();
+                string Freguesia = example.Records.NIF.Geo.Parish;
+                Inserir_Freguesia.Text = Freguesia.ToString();
+                string Email = example.Records.NIF.Contacts.Email;
+                Inserir_Email.Text = Email.ToString();
+                string Telefone = example.Records.NIF.Contacts.Phone;
+                Inserir_Telefone.Text = Telefone.ToString();
+                string Website = example.Records.NIF.Contacts.Website;
+                Inserir_Website.Text = Website.ToString();
+                string Fax = example.Records.NIF.Contacts.Fax;
+                Inserir_Fax.Text = Fax.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (example != null)
+            {
+
+            }
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png|All files(*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                Inserir_Imagem_URL.Text = openFileDialog1.FileName;
+            }
+            if (Inserir_NIF.Text == "504213660")
+            {
+                Inserir_Imagem_URL.Text = @"C:\Users\Diogo Major Vieira\Desktop\Curso Profissional\12º ano\Projeto1_NIF_Form\Projeto1_NIF_Form\Logos_Empresas\tgn.png";
+            }
+            else if (Inserir_NIF.Text == "504783661")
+            {
+                Inserir_Imagem_URL.Text = @"C:\Users\Diogo Major Vieira\Desktop\Curso Profissional\12º ano\Projeto1_NIF_Form\Projeto1_NIF_Form\Logos_Empresas\sas.png";
+            }
+            else if (Inserir_NIF.Text == "509442013")
+            {
+                Inserir_Imagem_URL.Text = @"C:\Users\Diogo Major Vieira\Desktop\Curso Profissional\12º ano\Projeto1_NIF_Form\Projeto1_NIF_Form\Logos_Empresas\nex.png";
+            }
+            else
+            {
+                Inserir_Imagem_URL.Text = @"C:\Users\Diogo Major Vieira\Desktop\Curso Profissional\12º ano\Projeto1_NIF_Form\Projeto1_NIF_Form\Logos_Empresas\tcm.png";
+            }
+        }
+
+        private void Gravar_Dados_Entity_Click(object sender, EventArgs e)
+        {
+            using (NIFDBEntities ndbe = new NIFDBEntities())
+            {
+                NIF_Empresa n_e = new NIF_Empresa();
+                {
+                    n_e.NIF = int.Parse(Inserir_NIF.Text);
+                    n_e.Name = Inserir_Nome.Text;
+                    n_e.Address = Inserir_Adereço.Text;
+                    n_e.PC4 = int.Parse(Inserir_PC4.Text);
+                    n_e.PC3 = int.Parse(Inserir_PC3.Text);
+                    n_e.Region = Inserir_Distrito.Text;
+                    n_e.County = Inserir_Concelho.Text;
+                    n_e.Parish = Inserir_Freguesia.Text;
+                    n_e.Email = Inserir_Email.Text;
+                    n_e.Phone = int.Parse(Inserir_Telefone.Text);
+                    n_e.Website = Inserir_Website.Text;
+                    n_e.Fax = int.Parse(Inserir_Fax.Text);
+                    n_e.Imagem_URL = Inserir_Imagem_URL.Text;
+                }
+            }
+        }
+    }
+}
---
+++ Projeto1_NIF_Form/NIF_Model.cs
@@ -0,0 +1,115 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Projeto1_NIF_Form
+{
+    public class Root
+    {
+        [JsonPropertyName("result")]
+        public string result { get; set; }
+        [JsonPropertyName("Records")]
+        public Records Records { get; set; }
+        [JsonPropertyName("nif_validation")]
+        public bool nif_validation { get; set; }
+        [JsonPropertyName("is_nif")]
+        public bool is_nif { get; set; }
+        [JsonPropertyName("credits")]
+        public Credits credits { get; set; }
+    }
+
+    public class Records
+    {
+        public NIF NIF { get; set; }
+    }
+
+    public class NIF
+    {
+        [JsonPropertyName("NIF")]
+        public int Nif { get; set; }
+        [JsonPropertyName("seo_url")]
+        public string seo_url { get; set; }
+        [JsonPropertyName("Title")]
+        public string Title { get; set; }
+        [JsonPropertyName("Address")]
+        public string Address { get; set; }
+        [JsonPropertyName("Pc4")]
+        public string Pc4 { get; set; }
+        [JsonPropertyName("Pc3")]
+        public string Pc3 { get; set; }
+        [JsonPropertyName("city")]
+        public string city { get; set; }
+        [JsonPropertyName("start_date")]
+        public string start_date { get; set; }
+        [JsonPropertyName("activity")]
+        public string activity { get; set; }
+        [JsonPropertyName("status")]
+        public string status { get; set; }
+        [JsonPropertyName("cae")]
+        public string[] cae { get; set; }
+        [JsonPropertyName("Contacts")]
+        public Contacts Contacts { get; set; }
+        [JsonPropertyName("structure")]
+        public Structure structure { get; set; }
+        [JsonPropertyName("Geo")]
+        public Geo Geo { get; set; }
+        [JsonPropertyName("Place")]
+        public Place Place { get; set; }
+        [JsonPropertyName("racius")]
+        public string racius { get; set; }
+        [JsonPropertyName("alias")]
+        public string alias { get; set; }
+        [JsonPropertyName("portugalio")]
+        public string portugalio { get; set; }
+    }
+
+    public class Contacts
+    {
+        [JsonPropertyName("Email")]
+        public string Email { get; set; }
+        [JsonPropertyName("Phone")]
+        public string Phone { get; set; }
+        [JsonPropertyName("Website")]
+        public string Website { get; set; }
+        [JsonPropertyName("Fax")]
+        public string Fax { get; set; }
+    }
+
+    public class Structure
+    {
+        [JsonPropertyName("nature")]
+        public string nature { get; set; }
+        [JsonPropertyName("capital")]
+        public string capital { get; set; }
+        [JsonPropertyName("capital_currency")]
+        public string capital_currency { get; set; }
+    }
+
+    public class Geo
+    {
+        [JsonPropertyName("Region")]
+        public string Region { get; set; }
+        [JsonPropertyName("County")]
+        public string County { get; set; }
+        [JsonPropertyName("Parish")]
+        public string Parish { get; set; }
+    }
+
+    public class Place
+    {
+        [JsonPropertyName("Address")]
+        public string Address { get; set; }
+        [JsonPropertyName("Pc4")]
+        public string Pc4 { get; set; }
+        [JsonPropertyName("Pc3")]
+        public string Pc3 { get; set; }
+        [JsonPropertyName("City")]
+        public string City { get; set; }
+    }
+
+    public class Credits
+    {
+        [JsonPropertyName("used")]
+        public string used { get; set; }
+        [JsonPropertyName("left")]
+        public object[] left { get; set; }
+    }
+}
