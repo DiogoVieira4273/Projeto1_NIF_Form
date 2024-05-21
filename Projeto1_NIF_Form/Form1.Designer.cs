7a6c2495a8e769d7cfa041421d4b1c7938deb8d8
Diogo Major Vieira <114163401+DiogoVieira4273@users.noreply.github.com>
2024-05-21 19:14:12 +0100

    Add project files.

---
+++ Projeto1_NIF_Form/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace Projeto1_NIF_Form
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Inserir_NIF = new System.Windows.Forms.TextBox();
+            this.Inserir_Nome = new System.Windows.Forms.TextBox();
+            this.Inserir_Adereço = new System.Windows.Forms.TextBox();
+            this.Inserir_PC4 = new System.Windows.Forms.TextBox();
+            this.Inserir_PC3 = new System.Windows.Forms.TextBox();
+            this.Inserir_Distrito = new System.Windows.Forms.TextBox();
+            this.Inserir_Concelho = new System.Windows.Forms.TextBox();
+            this.Inserir_Freguesia = new System.Windows.Forms.TextBox();
+            this.Inserir_Email = new System.Windows.Forms.TextBox();
+            this.Inserir_Telefone = new System.Windows.Forms.TextBox();
+            this.Inserir_Website = new System.Windows.Forms.TextBox();
+            this.Inserir_Fax = new System.Windows.Forms.TextBox();
+            this.Inserir_Imagem_URL = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Gravar_Dados_Entity = new System.Windows.Forms.Button();
+            this.Gravar_Dados_JSON = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Adereço";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PC4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "PC3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Distrito";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Concelho";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 207);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Freguesia";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 236);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Email";
+            // 
+            // Inserir_NIF
+            // 
+            this.Inserir_NIF.Location = new System.Drawing.Point(91, 10);
+            this.Inserir_NIF.Name = "Inserir_NIF";
+            this.Inserir_NIF.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_NIF.TabIndex = 9;
+            // 
+            // Inserir_Nome
+            // 
+            this.Inserir_Nome.Location = new System.Drawing.Point(91, 39);
+            this.Inserir_Nome.Name = "Inserir_Nome";
+            this.Inserir_Nome.Size = new System.Drawing.Size(298, 20);
+            this.Inserir_Nome.TabIndex = 10;
+            // 
+            // Inserir_Adereço
+            // 
+            this.Inserir_Adereço.Location = new System.Drawing.Point(91, 66);
+            this.Inserir_Adereço.Name = "Inserir_Adereço";
+            this.Inserir_Adereço.Size = new System.Drawing.Size(298, 20);
+            this.Inserir_Adereço.TabIndex = 11;
+            // 
+            // Inserir_PC4
+            // 
+            this.Inserir_PC4.Location = new System.Drawing.Point(91, 95);
+            this.Inserir_PC4.Name = "Inserir_PC4";
+            this.Inserir_PC4.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_PC4.TabIndex = 12;
+            // 
+            // Inserir_PC3
+            // 
+            this.Inserir_PC3.Location = new System.Drawing.Point(91, 124);
+            this.Inserir_PC3.Name = "Inserir_PC3";
+            this.Inserir_PC3.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_PC3.TabIndex = 13;
+            // 
+            // Inserir_Distrito
+            // 
+            this.Inserir_Distrito.Location = new System.Drawing.Point(91, 150);
+            this.Inserir_Distrito.Name = "Inserir_Distrito";
+            this.Inserir_Distrito.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_Distrito.TabIndex = 14;
+            // 
+            // Inserir_Concelho
+            // 
+            this.Inserir_Concelho.Location = new System.Drawing.Point(91, 176);
+            this.Inserir_Concelho.Name = "Inserir_Concelho";
+            this.Inserir_Concelho.Size = new System.Drawing.Size(163, 20);
+            this.Inserir_Concelho.TabIndex = 15;
+            // 
+            // Inserir_Freguesia
+            // 
+            this.Inserir_Freguesia.Location = new System.Drawing.Point(91, 204);
+            this.Inserir_Freguesia.Name = "Inserir_Freguesia";
+            this.Inserir_Freguesia.Size = new System.Drawing.Size(163, 20);
+            this.Inserir_Freguesia.TabIndex = 16;
+            // 
+            // Inserir_Email
+            // 
+            this.Inserir_Email.Location = new System.Drawing.Point(91, 233);
+            this.Inserir_Email.Name = "Inserir_Email";
+            this.Inserir_Email.Size = new System.Drawing.Size(163, 20);
+            this.Inserir_Email.TabIndex = 17;
+            // 
+            // Inserir_Telefone
+            // 
+            this.Inserir_Telefone.Location = new System.Drawing.Point(91, 259);
+            this.Inserir_Telefone.Name = "Inserir_Telefone";
+            this.Inserir_Telefone.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_Telefone.TabIndex = 19;
+            // 
+            // Inserir_Website
+            // 
+            this.Inserir_Website.Location = new System.Drawing.Point(91, 285);
+            this.Inserir_Website.Name = "Inserir_Website";
+            this.Inserir_Website.Size = new System.Drawing.Size(163, 20);
+            this.Inserir_Website.TabIndex = 20;
+            // 
+            // Inserir_Fax
+            // 
+            this.Inserir_Fax.Location = new System.Drawing.Point(91, 311);
+            this.Inserir_Fax.Name = "Inserir_Fax";
+            this.Inserir_Fax.Size = new System.Drawing.Size(100, 20);
+            this.Inserir_Fax.TabIndex = 21;
+            // 
+            // Inserir_Imagem_URL
+            // 
+            this.Inserir_Imagem_URL.Location = new System.Drawing.Point(91, 337);
+            this.Inserir_Imagem_URL.Name = "Inserir_Imagem_URL";
+            this.Inserir_Imagem_URL.Size = new System.Drawing.Size(665, 20);
+            this.Inserir_Imagem_URL.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 266);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Telefone";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Website";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(13, 318);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(24, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Fax";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 344);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(72, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Imagem_URL";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(271, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 190);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Gravar_Dados_Entity
+            // 
+            this.Gravar_Dados_Entity.Location = new System.Drawing.Point(559, 6);
+            this.Gravar_Dados_Entity.Name = "Gravar_Dados_Entity";
+            this.Gravar_Dados_Entity.Size = new System.Drawing.Size(85, 27);
+            this.Gravar_Dados_Entity.TabIndex = 28;
+            this.Gravar_Dados_Entity.Text = "Gravar_Entity";
+            this.Gravar_Dados_Entity.UseVisualStyleBackColor = true;
+            this.Gravar_Dados_Entity.Click += new System.EventHandler(this.Gravar_Dados_Entity_Click);
+            // 
+            // Gravar_Dados_JSON
+            // 
+            this.Gravar_Dados_JSON.Location = new System.Drawing.Point(428, 9);
+            this.Gravar_Dados_JSON.Name = "Gravar_Dados_JSON";
+            this.Gravar_Dados_JSON.Size = new System.Drawing.Size(85, 24);
+            this.Gravar_Dados_JSON.TabIndex = 29;
+            this.Gravar_Dados_JSON.Text = "Gravar_JSON";
+            this.Gravar_Dados_JSON.UseVisualStyleBackColor = true;
+            this.Gravar_Dados_JSON.Click += new System.EventHandler(this.Gravar_Dados_JSON_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 366);
+            this.Controls.Add(this.Gravar_Dados_JSON);
+            this.Controls.Add(this.Gravar_Dados_Entity);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Inserir_Imagem_URL);
+            this.Controls.Add(this.Inserir_Fax);
+            this.Controls.Add(this.Inserir_Website);
+            this.Controls.Add(this.Inserir_Telefone);
+            this.Controls.Add(this.Inserir_Email);
+            this.Controls.Add(this.Inserir_Freguesia);
+            this.Controls.Add(this.Inserir_Concelho);
+            this.Controls.Add(this.Inserir_Distrito);
+            this.Controls.Add(this.Inserir_PC3);
+            this.Controls.Add(this.Inserir_PC4);
+            this.Controls.Add(this.Inserir_Adereço);
+            this.Controls.Add(this.Inserir_Nome);
+            this.Controls.Add(this.Inserir_NIF);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Inserir_NIF;
+        private System.Windows.Forms.TextBox Inserir_Nome;
+        private System.Windows.Forms.TextBox Inserir_Adereço;
+        private System.Windows.Forms.TextBox Inserir_PC4;
+        private System.Windows.Forms.TextBox Inserir_PC3;
+        private System.Windows.Forms.TextBox Inserir_Distrito;
+        private System.Windows.Forms.TextBox Inserir_Concelho;
+        private System.Windows.Forms.TextBox Inserir_Freguesia;
+        private System.Windows.Forms.TextBox Inserir_Email;
+        private System.Windows.Forms.TextBox Inserir_Telefone;
+        private System.Windows.Forms.TextBox Inserir_Website;
+        private System.Windows.Forms.TextBox Inserir_Fax;
+        private System.Windows.Forms.TextBox Inserir_Imagem_URL;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Gravar_Dados_Entity;
+        private System.Windows.Forms.Button Gravar_Dados_JSON;
+    }
+}
